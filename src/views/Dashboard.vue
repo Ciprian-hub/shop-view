efe8b83e346702ec31098e4d4a881e4b9612324a
Ciprian-hub <cipssul@gmail.com>
2023-11-08 15:58:48 +0200

    add dashboard-wip

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,79 @@
 <template>
-  <div class="flex items-center justify-between mb-3">
-    <h1 class="text-3xl font-semibold">Dashboard</h1>
-    <button type="button"
-            @click="showAddNewModal()"
-            class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      Add new Product
-    </button>
+  <h1 class="text-3xl font-semibold">Dashboard</h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
+    <div class="flex flex-col items-center bg-white py-3 rounded-md">
+      <div>
+        Nr of customers
+      </div>
+      <div>
+        {{ customersCount }}
+      </div>
+    </div>
+    <div class="flex flex-col items-center bg-white py-3 rounded-md">
+      <div>
+        productsCount
+      </div>
+      <div>
+        {{ productsCount }}
+      </div>
+    </div>
+    <div class="flex flex-col items-center bg-white py-3 rounded-md">
+      <div>
+        paidOrders
+      </div>
+      <div>
+        {{ paidOrders }}
+      </div>
+    </div>
+    <div class="flex flex-col items-center bg-white py-3 rounded-md">
+      <div>
+        totalAmount
+      </div>
+      <div>
+        {{ totalAmount }} $
+      </div>
+    </div>
+  </div>
+  <div class="grid grid-rows-2 grid-cols-2 grid-flow-col gap-4 md:grid-cols-2">
+    <div class="row-span-3 bg-white">
+<!--      <Doughnut/>-->
+    </div>
+    <div class="col-span-2 bg-white">
+      Somethin
+    </div>
+    <div class="row-span-2 col-span-2 bg-white">
+      Something
+    </div>
   </div>
 </template>
 
 <script setup>
+import Doughnut from "../components/Charts/Doughnut.vue";
+import axiosClient from "../axios";
+import {ref} from "vue";
+
+const customersCount = ref(0)
+const productsCount = ref(0)
+const paidOrders = ref(0)
+const totalAmount = ref(0)
+
+axiosClient.get('dashboard/customers')
+    .then(({data}) => {
+      customersCount.value = data
+    })
+axiosClient.get('dashboard/products')
+    .then(({data}) => {
+      productsCount.value = data
+    })
+axiosClient.get('dashboard/orders')
+    .then(({data}) => {
+      paidOrders.value = data
+    })
+axiosClient.get('dashboard/total')
+    .then(({data}) => {
+      totalAmount.value = new Intl.NumberFormat('en-EU', {style: 'currency', currency: 'EUR'}).format(data)
+    })
 
 </script>
 
